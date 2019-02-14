fe3b60886c05006ecea48b7be050cd0f7545fdef
Brice Chapuis <brice.chapuis@gmail.com>
2019-02-14 15:55:51 +0100

    rake done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Generating ingredients...'
+content = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+json = JSON.parse(content)
+json['drinks'].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+puts 'Done!'
